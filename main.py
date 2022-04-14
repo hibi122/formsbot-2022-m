98bb799099a81fb7b4d89588807abc5e10db1859
hibimori <hibimori122@gmail.com>
2022-04-14 14:52:01 +0900

    make it better

---
+++ main.py
@@ -0,0 +1,14 @@
+import imp
+from operator import imod
+from flask import Flask, request, abort,jsonify
+import os
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
