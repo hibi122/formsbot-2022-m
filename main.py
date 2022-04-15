82e6ec63b7fe5565570813679b6f9305a7af39b9
hibimori <hibimori122@gmail.com>
2022-04-15 09:57:42 +0900

    six

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, abort,jsonify
+from flask import Flask, request, abort, jsonify
 import os
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+
 
 app = Flask(__name__)
 
